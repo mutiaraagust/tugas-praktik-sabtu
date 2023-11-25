5308707bfbb6de4680454fd8b3ed9445ba4c5b08
mutiaraagust <147906556+mutiaraagust@users.noreply.github.com>
2023-11-25 15:01:59 +0700

    Add files via upload

---
+++ MODUL 6/TUGAS 6 NO 4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+	int kecepatan = 2;
+	int waktu = 100;
+	int jarak = kecepatan *waktu;
+	
+	printf("jarak yang ditempuh setelah %d detik : %d meter \n", waktu,jarak);
+	return 0;
+}
